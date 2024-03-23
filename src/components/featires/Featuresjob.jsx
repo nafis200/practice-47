c6ba7fcc5c3964673b4ce0c251aabe79d48ee577
Nafis Ahamed <nafisahamed14@gmail.com>
2024-03-23 22:14:02 +0600

    eight commit

--- src/components/featires/Featuresjob.jsx
+++ src/components/featires/Featuresjob.jsx
@@ -4,6 +4,8 @@ import Job from "../job/Job";
 
 const Featuresjob = () => {
      const [jobs,setJobs] = useState([])
+     const [datalength, setDatalength] = useState(4)
+
      useEffect(()=>{
          fetch('jobs.json')
          .then(res => res.json())
@@ -15,8 +17,11 @@ const Featuresjob = () => {
             <p className="mt-8">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium, nesciunt.</p>
             <div className="grid grid-cols-2 gap-6 mt-5">
                 {
-                    jobs.map(job => <Job key={job.id} job={job} ></Job> )
+                    jobs.slice(0,datalength).map(job => <Job key={job.id} job={job} ></Job> )
                 }
+            </div>
+            <div className={datalength === jobs.length && 'hidden'}>
+                <button onClick={()=>setDatalength(jobs.length)} className="btn btn-primary mt-5">Show all jobs</button>
             </div>
 
         </div>
