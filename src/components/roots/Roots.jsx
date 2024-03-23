0e8e72f376e1b00b61e36e37bbca73d4b91128f8
Nafis Ahamed <nafisahamed14@gmail.com>
2024-03-23 20:55:32 +0600

    second commit

--- src/components/roots/Roots.jsx
+++ src/components/roots/Roots.jsx
@@ -1,9 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../footer/Footer";
+import Header from "../header/Header";
 
 
 const Roots = () => {
     return (
         <div>
             <h1>hellow world</h1>
+            <Header></Header>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
