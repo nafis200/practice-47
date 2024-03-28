0657df5361adb357b90e378b6f5bd6b38145f114
Nafis Ahamed <nafisahamed14@gmail.com>
2024-03-28 21:58:51 +0600

    add readme

--- src/components/appliedjobs/AppliedJobs.jsx
+++ src/components/appliedjobs/AppliedJobs.jsx
@@ -2,37 +2,47 @@ import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { getStoredJob } from "../../utility/Localstorage";
 import { Helmet } from "react-helmet-async";
+import GoogleMapReact from 'google-map-react';
+import { googleapi } from "./Googleapi";
 
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 
 const AppliedJobs = () => {
 
-    const jobs = useLoaderData()
+  const defaultProps = {
+    center: {
+      lat: 10.99835602,
+      lng: 77.01502627
+    },
+    zoom: 11
+  };
 
-    const [appliedJobs, setAppliedjobs] = useState([])
-
-    useEffect(()=>{
-        const storedJobsIds = getStoredJob()
-        if(jobs.length > 0){
-           const jobsApplied = [];
-           for(const id of storedJobsIds){
-             const job = jobs.find(job => job.id === id);
-             if(job){
-                jobsApplied.push(job)
-             }
-           }
-        //    console.log(jobs,storedJobsIds,jobsApplied);
-        setAppliedjobs(jobsApplied)
-        }
-    },[])
-    
-
-    return (
-        <div>
-<Helmet> <title>nafis</title> </Helmet>
-            <h2 className="text-2xl">Jobs I applied:{appliedJobs.length}</h2>
+  return (
+    <div>
+      <Helmet>
+        <title>nafis</title>
+      </Helmet>
+      <h2 className="text-2xl">Jobs I applied:</h2>
+      
+      <section>
+        <div style={{ height: "100vh", width: "100%" }}>
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: googleapi  }}
+            defaultCenter={defaultProps.center}
+            defaultZoom={defaultProps.zoom}
+          >
+            <AnyReactComponent
+              lat={59.955413}
+              lng={30.337844}
+              text="My Marker"
+            />
+          </GoogleMapReact>
         </div>
-    );
+      </section>
+
+    </div>
+  );
 };
 
 export default AppliedJobs;
